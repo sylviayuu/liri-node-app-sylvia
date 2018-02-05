d4c35b75962a55ad5622c743a479fbb99b8ddd82
Sylvia Yu <sylviayu7@gmail.com>
2018-02-04 22:57:38 -0600

    hw submit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  consumer_key: 'HAetz59jYy3O8YIGEpJ9Xjxrx',
+  consumer_secret: 'XpQegEyM0YUflzewmvfOg1b1GMNGBzmgf5ASsrQQEGN9ivOnzh',
+  access_token_key: '960048605754163201-KEBQ2tGhPtvFmbeKprdLPoUjDLdojtz',
+  access_token_secret: 'SuHVUXABGioQE5UoIZd73SzTESDbKpO147qABxQw0WQ8y'
---
+++ liri.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+
+console.log('this is loaded');
+
+
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
+
+var retweet = function() {
+    var params = {
+        q: '#nodejs, #Nodejs',  // REQUIRED
+        result_type: 'recent',
+        lang: 'en'
+    }
+    // for more parameters, see: https://dev.twitter.com/rest/reference/get/search/tweets
+
+    Twitter.get('search/tweets', params, function(err, data) {
+      // if there no errors
+        if (!err) {
+          // grab ID of tweet to retweet
+            var retweetId = data.statuses[0].id_str;
+            // Tell TWITTER to retweet
+            Twitter.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Retweeted!!!');
+                }
+                // if there was an error while tweeting
+                if (err) {
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                }
+            });
+        }
+        // if unable to Search a tweet
+        else {
+          console.log('Something went wrong while SEARCHING...');
+        }
+    });
+}
+
+var spotify = require('spotify');
+ 
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    // Do something with 'data' 
+});
+
+
+// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+var request = require("request");
+
+// Then run a request to the OMDB API with the movie specified
+request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+  // If the request is successful (i.e. if the response status code is 200)
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Nsote: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+  }
+});
+
